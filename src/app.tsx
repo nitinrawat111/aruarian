36811be4fa150df8d0174ec6b18b63bd673d817c
nitinrawat111 <rawat.nitin111@gmail.com>
2026-01-18 17:45:52 +0530

    init: configure mantine

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,11 @@
+import { MantineProvider, Text } from "@mantine/core";
+import "@mantine/core/styles.css";
+import "@mantine/spotlight/styles.css";
+
 export function App() {
-  return <h1>Hello</h1>;
+  return (
+    <MantineProvider>
+      <Text>Hello</Text>
+    </MantineProvider>
+  );
 }
