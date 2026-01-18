7f9fdfcdfd39862351783d101d048aa9fbf0f660
nitinrawat111 <rawat.nitin111@gmail.com>
2026-01-18 15:09:11 +0530

    init: flakes

---
+++ flake.nix
@@ -0,0 +1,57 @@
+# Reference: https://v1.tauri.app/v1/guides/getting-started/prerequisites/#setting-up-linux
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        libraries = with pkgs;[
+          webkitgtk_4_1
+          gtk3
+          cairo
+          gdk-pixbuf
+          glib
+          dbus
+          openssl
+          librsvg
+        ];
+
+        packages = with pkgs; [
+          # Rust
+          rustc
+          rustfmt
+          clippy
+          cargo
+
+          # Node.js 24
+          nodejs_24
+
+          curl
+          wget
+          pkg-config
+          dbus
+          openssl
+          glib
+          gtk3
+          libsoup_3
+          webkitgtk_4_1
+          librsvg
+        ];
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = packages;
+
+          shellHook =
+            ''
+              export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
+              export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
+            '';
+        };
+      });
+}
