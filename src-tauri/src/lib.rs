630d29ca9568491fcb2057171f3df8095aa0286f
nitinrawat111 <rawat.nitin111@gmail.com>
2026-01-18 23:48:48 +0530

    feat: implement basic workflow for hiding/showing launcher window

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,4 @@
+use tauri::Manager;
 use tauri_plugin_global_shortcut::{Code, Modifiers};
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
@@ -16,10 +17,14 @@ pub fn run() {
         // with_shortcuts only panics when the provided shortcut string is invalid
         // This will never panic (source: trust me bro)
         .unwrap()
-        .with_handler(|app, shortcut, event| {
+        .with_handler(|app, shortcut, _event| {
             if shortcut.matches(Modifiers::SUPER, Code::Space) {
-                // TODO: Create window here
-                println!("shortcut pressed");
+                // Open the launcher window when the shortcut is pressed
+                // "launcher" is the label of the window defined in tauri.conf.json
+                app.get_webview_window("launcher")
+                    .expect("Failed to get launcher window")
+                    .show()
+                    .expect("Failed to show launcher window");
             }
         })
         .build();
@@ -28,6 +33,29 @@ pub fn run() {
         .plugin(global_shortcut_plugin)
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
+        .setup(|app| {
+            // Earlier i thought of opening a new window when the shortcut is pressed and then closing it
+            // But that seemed expensive, so instead decided to hide/unhide the same window
+
+            // "launcher" is the label of the window defined in tauri.conf.json
+            let launcher_window = app
+                .get_webview_window("launcher")
+                .expect("Failed to get launcher window");
+            // Hide the launcher window on startup
+            launcher_window
+                .hide()
+                .expect("Failed to hide launcher window on startup");
+
+            // Also attach an event listener to hide the launcher window when it loses focus
+            launcher_window.clone().on_window_event(move |event| {
+                if let tauri::WindowEvent::Focused(false) = event {
+                    launcher_window
+                        .hide()
+                        .expect("Failed to hide launcher window");
+                }
+            });
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
