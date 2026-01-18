bbb3d41e52b5161186b5b90799b8cd056942a69b
nitinrawat111 <rawat.nitin111@gmail.com>
2026-01-18 17:23:48 +0530

    chore: pretty

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <h1 className="text-3xl font-bold">Hello</h1>
-  );
+  return <h1 className="text-3xl font-bold">Hello</h1>;
 }
 
 export default App;
