879bddace45527de74a92d4b805656ef50295a2d
nitinrawat111 <rawat.nitin111@gmail.com>
2026-01-18 22:59:18 +0530

    feat: add global shortcut for launcher

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,5 @@
+use tauri_plugin_global_shortcut::{Code, Modifiers};
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -6,7 +8,24 @@ fn greet(name: &str) -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    // Register the global shortcut for the launcher
+    // Reference: https://crates.io/crates/tauri-plugin-global-shortcut
+    let global_shortcut_plugin = tauri_plugin_global_shortcut::Builder::new()
+        .with_shortcuts(["Super+Space"])
+        // It's ok to use unwrap here
+        // with_shortcuts only panics when the provided shortcut string is invalid
+        // This will never panic (source: trust me bro)
+        .unwrap()
+        .with_handler(|app, shortcut, event| {
+            if shortcut.matches(Modifiers::SUPER, Code::Space) {
+                // TODO: Create window here
+                println!("shortcut pressed");
+            }
+        })
+        .build();
+
     tauri::Builder::default()
+        .plugin(global_shortcut_plugin)
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
