d4bf6d57f82417f46fdc1473f83b2c07a0cab654
nitinrawat111 <rawat.nitin111@gmail.com>
2026-01-18 17:32:12 +0530

    rollback: i'll go with mantine

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,3 @@
-import "./app.css";
-
 export function App() {
-  return <h1 className="text-3xl font-bold">Hello</h1>;
+  return <h1>Hello</h1>;
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 // @ts-expect-error process is a nodejs global
@@ -7,7 +6,7 @@ const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
