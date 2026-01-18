af67dba6918595764160220bea17ecd8c793f0e2
nitinrawat111 <rawat.nitin111@gmail.com>
2026-01-18 17:22:04 +0530

    init: tailwind + aliases + shadcdn setup

--- flake.nix
+++ flake.nix
@@ -1,56 +1,54 @@
 # Reference: https://v1.tauri.app/v1/guides/getting-started/prerequisites/#setting-up-linux
 {
   inputs = {
-    nixpkgs.url = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
+    fenix.url = "github:nix-community/fenix";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, fenix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
 
-        libraries = with pkgs;[
-          webkitgtk_4_1
-          gtk3
-          cairo
-          gdk-pixbuf
-          glib
-          dbus
-          openssl
-          librsvg
+        rustToolchain = fenix.packages.${system}.stable.withComponents [
+          "rustc"
+          "cargo"
+          "rustfmt"
+          "clippy"
+          "rust-src"
         ];
-
-        packages = with pkgs; [
-          # Rust
-          rustc
-          rustfmt
-          clippy
-          cargo
-
-          # Node.js 24
-          nodejs_24
-
-          curl
-          wget
-          pkg-config
-          dbus
-          openssl
-          glib
-          gtk3
-          libsoup_3
-          webkitgtk_4_1
-          librsvg
-        ];
-      in
-      {
+      in {
         devShell = pkgs.mkShell {
-          buildInputs = packages;
+          buildInputs = with pkgs; [
+            rustToolchain
+            rust-analyzer
 
-          shellHook =
-            ''
-              export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
-            '';
+            # Node.js
+            nodejs_24
+
+            # Build tools
+            pkg-config
+            curl
+            wget
+
+            # Tauri deps
+            dbus
+            glib
+            gtk3
+            libsoup_3
+            webkitgtk_4_1
+            openssl
+            librsvg
+            gdk-pixbuf
+            cairo
+          ];
+
+          shellHook = ''
+            export RUST_SRC_PATH="${rustToolchain}/lib/rustlib/src/rust/library"
+            export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
+          '';
         };
-      });
+      }
+    );
 }
